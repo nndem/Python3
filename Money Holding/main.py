b0c2376a57eb879c90f5a4c94837fa4d6402c56b
Nick <demidov_nn@inbox.ru>
2020-04-05 19:26:39 +0300

    create toolbar, button and childWindow

--- Money Holding/main.py
+++ Money Holding/main.py
@@ -4,6 +4,33 @@ import tkinter as tk
 class Main(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
+        self.init_main()
+
+    def init_main(self):
+        toolbar = tk.Frame(bg="#d7dBe0", bd=2)
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+
+        btn_open_dialog = tk.Button(toolbar, text='Добавить позицию',
+                                    command=self.open_dialog, bg="#d7dBe0", bd=0,
+                                    compound=tk.TOP)
+        btn_open_dialog.pack(side=tk.LEFT)
+
+    @staticmethod
+    def open_dialog():
+        Child()
+
+class Child(tk.Toplevel):
+    def __init__(self):
+        super().__init__(root)
+        self.init_child()
+
+    def init_child(self):
+        self.title("Добавить доходы/расходы")
+        self.geometry('400x200+400+300')
+        self.resizable(False, False)
+
+        self.grab_set()
+        self.focus_set()
 
 
 if __name__ == "__main__":
@@ -14,4 +41,3 @@ if __name__ == "__main__":
     root.geometry("650x450+300+200")
     root.resizable(False, False)
     root.mainloop()
-    

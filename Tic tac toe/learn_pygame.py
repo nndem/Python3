8cabe30004ddc69244772b1cf6a3152338969ab8
Nick <demidov_nn@inbox.ru>
2020-05-04 19:21:13 +0300

    fonts, text

--- Tic tac toe/learn_pygame.py
+++ Tic tac toe/learn_pygame.py
@@ -1,11 +1,21 @@
 import pygame
 
+pygame.init()
 # создаем окно программы
 size = (600, 400)
-pygame.display.set_mode(size)
+screen = pygame.display.set_mode(size)
 pygame.display.set_caption('My Programme')
 img = pygame.image.load('icon.jpg')
 pygame.display.set_icon(img)
+
+# работа со шрифтами
+font = pygame.font.SysFont('malgungothic', 32)
+
+# создание текста
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+follow = font.render('alive!', 1, RED, GREEN)
 
 
 ### игровой цикл (цикл обработки событий)
@@ -15,3 +25,5 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+    screen.blit(follow, (0, 0))
+    pygame.display.update()

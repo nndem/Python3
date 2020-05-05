fb1f99c72be14edd5d4031cee803ce43425735b9
Nick <demidov_nn@inbox.ru>
2020-05-05 18:32:48 +0300

    tic-tac-toe game

--- Tic tac toe/game_field.py
+++ Tic tac toe/game_field.py
@@ -6,11 +6,11 @@ pygame.init()
 size = (500, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Game field")
-width = heigth = 40
+width = height = 40
 margin = 10
 red = (255, 0, 0)
 white = (255, 255, 255)
-mas = [[0]*10 for i in range(10)]
+mas = [[0] * 10 for i in range(10)]
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -19,8 +19,8 @@ while True:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x_mouse, y_mouse = pygame.mouse.get_pos()
             print(f'x={x_mouse} y={y_mouse}')
-            column = x_mouse//(margin+width)
-            row = y_mouse // (margin+heigth)
+            column = x_mouse // (margin + width)
+            row = y_mouse // (margin + height)
             mas[row][column] = mas[row][column] ^ 1
     # рисование квадратика
     for row in range(10):
@@ -29,7 +29,7 @@ while True:
                 color = red
             else:
                 color = white
-            x = col*width + (col+1)*margin
-            y = row * heigth + (row + 1)*margin
-            pygame.draw.rect(screen, color, (x, y, width, heigth))
+            x = col * width + (col + 1) * margin
+            y = row * height + (row + 1) * margin
+            pygame.draw.rect(screen, color, (x, y, width, height))
     pygame.display.update()

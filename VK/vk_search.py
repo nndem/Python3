614f21513dea7ea0ce0e4695f8ea99d35b2f3c69
Nick <demidov_nn@inbox.ru>
2020-05-09 11:44:20 +0300

    filter, write to catalog

--- VK/vk_search.py
+++ VK/vk_search.py
@@ -1,12 +1,17 @@
-import vk_api, time
+import vk_api
+import time
 import codecs
-import os, sys, shutil, requests
-import os.path
+
 
 timestamp = int(time.time())
 
 
-class VkSearcher():
+class VkSearcher:
+
+    def __init__(self):
+        # self.catalogue = codecs.open('girls.html', 'w', encoding='utf8')
+        self.autorization('login.txt', 'pass.txt')
+        self.catalogue = codecs.open('girls.html', 'w', encoding='utf8')
 
     def autorization(self, login_file: str, password_file: str):
         with open(login_file) as login_file:
@@ -33,34 +38,41 @@ class VkSearcher():
                 age_range: {self.params['age_min']}-{self.params['age_max']}\n
                       sex: {self.params['pol']}\n
                 """)
-        min_vozr = self.params['age_min']
-        max_vozr = self.params['age_max']
-        city_number = self.params['location']
-        sex = self.params['pol']
+        min_vozr, max_vozr = self.params['age_min'], self.params['age_max']
+        city_number, sex = self.params['location'], self.params['pol']
         while min_vozr <= max_vozr:
             month_number = 1
-            result = self.vk.users.search(
-                count=1000,
-                fields='id, photo_max_orig, has_photo, last_seen, relation', city=city_number, sex=sex
-            )
-            month_number += 1
-            print(result)
-            # filtering
+            while month_number <=12:
+                self.result = self.vk.users.search(
+                    count=10,
+                    fields='id, photo_max_orig, has_photo, last_seen, relation',
+                    city=city_number, sex=sex, age_from=min_vozr, age_to=max_vozr, birth_month=month_number
+                                            )
+                self.result_filter(self.result)# filtering
+                month_number += 1
+            min_vozr += 1
 
+    def result_filter(self, result):
+        for item in result['items']:
+            if 'relation' in item and item['relation'] == 0 and item['has_photo'] == True\
+            and 'last_seen' in item:
+                not_far = timestamp-int(item['last_seen']['time'])
+                if not_far < 2592000:
+                    info = '<a href="https://vk.com/id'+str(item['id'])+\
+                           '" target="_blank" style="width: 302px; height: 302px;' \
+                           'display: inline-block;"><div style="display: inline-block;' \
+                           ' background: url('+str(item['photo_max_orig'])+');' \
+                           ' width: 300px; height: 300px; background-size: cover; ' \
+                           'background-position: center center;"></div></a>'+'\n'
+                    self.write_to_catalogue(self.catalogue, info)
+                    print('https://vk.com/id' + str(item['id']))
 
-
-
-
-
-
-
-
-
-
+    def write_to_catalogue(self, catalogue, info):
+        catalogue.write(info)
+        return catalogue
 
 def main():
     obj = VkSearcher()
-    obj.autorization('login.txt', 'pass.txt')
     obj.enter_parameters([20, 21], city_number=123, sex=1)
     obj.search()
 
